6c038a5e9366d470dd7fbd14f41e66b2f90f8b9a
syahpiar <leadpiar312@gmail.com>
2026-08-05 16:43:24 +0700

    Create flower.js

---
+++ flower.js
@@ -0,0 +1,25 @@
+const flowers=document.getElementById("flowers");
+
+if(flowers){
+
+for(let i=0;i<20;i++){
+
+const flower=document.createElement("div");
+
+flower.className="flower";
+
+flower.innerHTML="🌸";
+
+flower.style.left=Math.random()*100+"vw";
+
+flower.style.fontSize=(18+Math.random()*20)+"px";
+
+flower.style.animationDuration=(8+Math.random()*8)+"s";
+
+flower.style.animationDelay=Math.random()*8+"s";
+
+flowers.appendChild(flower);
+
+}
+
+}
